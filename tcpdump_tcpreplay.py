bfd8853f948c0a21401778ff5df3898d6785cd68
Aymen Jemi <jemiaymen@gmail.com>
2022-05-04 23:26:30 +0100

    update tcp pod code

--- tcpdump_tcpreplay.py
+++ tcpdump_tcpreplay.py
@@ -1,11 +1,10 @@
 import subprocess
-from time import sleep
 from scapy.all import *
 
 import random
 import socket
 
-iface = 'wlp0s20f3'
+iface = 'eth0'
 dport = 60000 
 ips = { 'sender' :'10.244.246.129',
         'ingress' : '10.244.246.130',
@@ -18,23 +17,27 @@ ips = { 'sender' :'10.244.246.129',
         'receiver':'10.244.246.137' }
 
 sport = random.randint(49152,65535)
-to1 = socket.gethostbyname(ips['ingress'])
-to2 = socket.gethostbyname(ips['uplink'])
 
-pkt = Ether(src=get_if_hwaddr(iface),dst='ee:ee:ee:ee:ee:ee') /IP(dst=to1) / TCP(dport=dport , sport=sport ) / "Test PKT"
-pkt2 = Ether(src=get_if_hwaddr(iface),dst='ee:ee:ee:ee:ee:ee') /IP(dst=to2) / TCP(dport=dport , sport=sport ) / "Test PKT Uplink"
+to = socket.gethostbyname(ips['ingress'])
 
-wrpcap("src.pcap",[pkt,pkt2])
+pkts = []
+for x in range(10):
+    pkt = Ether(src=get_if_hwaddr(iface),dst='ee:ee:ee:ee:ee:ee') /IP(dst=to) / TCP(dport=dport , sport=sport ) / "Test PKT {0}".format(x) 
+    pkts.append(pkt)
 
-tcpreplay_cli = "tcpreplay -i {0} --topspeed src.pcap".format(iface)
 
-tcpdump_cli = "tcpdump -tt -i {0} src host {1} > dst.pcap".format(iface,ips['sender'])
+wrpcap("/app/src.pcap",pkts)
+
+tcpreplay_cli = "tcpreplay -i {0} --topspeed /app/src.pcap".format(iface)
+
+
 
 tcpreplay = subprocess.Popen(tcpreplay_cli,shell=True)
 # tcpreplay.wait()
 
 # sleep(0.3)
 
+# tcpdump_cli = "tcpdump -tt -i {0} src host {1} > dst.pcap".format(iface,ips['sender'])
 # tcpdump = subprocess.Popen(tcpdump_cli , shell=True)
 # tcpdump.wait()
 
@@ -42,6 +45,9 @@ def handle_pkt(pkt):
     pkt.show2()
     sys.stdout.flush()
 
-final_pkts = sniff(count=2,filter="tcp and port {0}".format(dport) , prn= lambda x: handle_pkt(x) )
+final_pkts = sniff(count=10,filter="tcp and port {0}".format(dport) , prn= lambda x: handle_pkt(x) )
 
-wrpcap("dst.pcap",final_pkts)
+wrpcap("/app/dst.pcap",final_pkts)
+
+latency = subprocess.Popen('/app/pcap_latency --latency-histo /app/src.pcap /app/dst.pcap',shell=True)
+latency.wait()
